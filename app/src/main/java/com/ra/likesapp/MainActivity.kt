05b5ecdf4da250a582c8111f81bb0be705863492
Ryan Ardyansyah <ui.ardyan@gmail.com>
2022-12-05 00:24:48 +0700

    make a face on draw

--- app/src/main/java/com/ra/likesapp/MainActivity.kt
+++ app/src/main/java/com/ra/likesapp/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.ra.likesapp
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import com.ra.likesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val mBitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
+    private val mCanvas = Canvas(mBitmap)
+    private val mPaint = Paint()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageView.setImageBitmap(mBitmap)
+        showFace()
+
+
     }
+
+    private val halfofWidth = (mBitmap.width / 2).toFloat()
+    private val halfofHeight = (mBitmap.height / 2).toFloat()
+
+    private val left = 150F
+    private val top = 250F
+    private val right = mBitmap.width - left
+    private val bottom = mBitmap.height.toFloat() - 50F
+
+    private fun showFace(){
+        val face = RectF(left, top, right, bottom)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_left_skin, null)
+        mCanvas.drawArc(face, 90F, 180F, false, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_right_skin, null)
+        mCanvas.drawArc(face, 270F, 180F, false, mPaint)
+    }
+
 }
